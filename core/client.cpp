c4e1f40b6774eacac80a53baebb60b1d887e34be
Loïc Bourgois <loicbourgois.web@gmail.com>
2015-05-05 19:12:24 +0200

    Handle downloading & uploading

--- core/client.cpp
+++ core/client.cpp
@@ -1,15 +1,16 @@
 #include "client.hpp"
 #include "util.hpp"
+#include <QFile>
 
 Client::Client(QString ip, int port) :
-  ip(ip),
-  port(port),
-  tcpSocket()
+    ip(ip),
+    port(port),
+    tcpSocket()
 {
-  QObject::connect(&tcpSocket, SIGNAL(readyRead()),
-                   this, SLOT(read()));
-  QObject::connect(&tcpSocket, SIGNAL(disconnected()),
-                   this, SLOT(onDisconnected()));
+    QObject::connect(&tcpSocket, SIGNAL(readyRead()),
+                     this, SLOT(read()));
+    QObject::connect(&tcpSocket, SIGNAL(disconnected()),
+                     this, SLOT(onDisconnected()));
 }
 
 Client::~Client()
@@ -19,59 +20,101 @@ Client::~Client()
 
 void Client::connect()
 {
-  tcpSocket.connectToHost(ip, port);
+    tcpSocket.connectToHost(ip, port);
+}
+
+void Client::disconnect()
+{
+    write("exit");
+}
+
+void Client::log(QString username, QString password)
+{
+    write("log " + username + " " + password);
 }
 
 void Client::onDisconnected()
 {
-
+    emit disconnected();
 }
 
 void Client::read()
 {
-  QString answer = tcpSocket.readAll();
-  answer = answer.left(answer.size()-2);
-  Util::addLog("Answer  : " + answer);
-  handleAnswer(answer);
+    QString answer = tcpSocket.readAll();
+    answer = answer.left(answer.size()-2);
+    Util::addLog("Answer  : " + answer);
+    handleAnswer(answer);
 }
 
 void Client::write(QString message)
 {
-  Util::addLog("Request : " + message);
-  QByteArray block;
-  message += "\r\n";
-  block.append(message);
-  tcpSocket.write(block);
+    Util::addLog("Request : " + message);
+    message += "\r\n";
+    tcpSocket.write(message.toUtf8());
+}
+
+void Client::askJob()
+{
+    write("getjob");
+}
+
+void Client::askBrain()
+{
+    write("getbrain");
+}
+
+void Client::sendBrain(QString brain)
+{
+    QString request = "sendbrain " + brain;
+    write(request);
 }
 
 void Client::handleAnswer(QString answer)
 {
-  if(answer == "hi")
-  {
-    /*write("log "
-          + connectionForm.ui->lineEditUsername->text()+ " "
-          + connectionForm.ui->lineEditPassword->text());*/
+    if(answer == "hi")
+    {
+        emit connectionEstablished();
+    }
+    else if(answer == "welcome")
+    {
+        emit logged();
+    }
+    else if(answer == "wtf")
+    {
+        Util::addLog("Server doesn't seem to understand the request");
+    }
+    else if(answer == "unicorn")
+    {
+        emit loginRefused();
+    }
+    else if(answer.startsWith("job"))
+    {
+        answer.remove(0,4);
+        QStringList strs = answer.split(' ');
+        int jobId = strs[0];
+        QString inputs = ef;
+        QString brain = ee;
+    }
+    else if(answer == "bye")
+    {
+        tcpSocket.disconnectFromHost();
+        emit disconnected();
+    }
+    else if(answer.startsWith("brain"))
+    {
+        QString brainXML = answer.remove(0,6);
+        QByteArray aa;
+        aa.append(brainXML);
+        QFile file("./test.brain");
+        file.open(QIODevice::ReadWrite);
+        file.write(aa);
+        file.close();
+    }
+    else if(answer == "brainreceived")
+    {
 
-  }
-  if(answer == "welcome")
-  {
-
-  }
-  if(answer == "wtf")
-  {
-    Util::addLog("Server doesn't seem to understand the request");
-  }
-  if(answer == "unicorn")
-  {
-
-  }
-  if(answer == "job")
-  {
-
-  }
-  if(answer == "bye")
-  {
-    // TODO disconnect
-  }
+    }
+    else
+    {
+    }
 }
-
--- core/client.hpp
+++ core/client.hpp
@@ -6,20 +6,30 @@
 
 class Client : public QObject
 {
-    Q_OBJECT
-  public:
-    Client(QString ip, int port);
-    ~Client();
-    void connect();
-    void write(QString message);
-  private slots:
-    void read();
-    void onDisconnected();
-  private:
-    void handleAnswer(QString answer);
-    QString ip;
-    int port;
-    QTcpSocket tcpSocket;
+        Q_OBJECT
+    public:
+        Client(QString ip, int port);
+        ~Client();
+        void connect();
+        void disconnect();
+        void log(QString username, QString password);
+        void write(QString message);
+        void askJob();
+        void askBrain();
+        void sendBrain(QString brain);
+    private slots:
+        void read();
+        void onDisconnected();
+    signals:
+        void connectionEstablished();
+        void loginRefused();
+        void logged();
+        void disconnected();
+    private:
+        void handleAnswer(QString answer);
+        QString ip;
+        int port;
+        QTcpSocket tcpSocket;
 };
 
 #endif // CLIENT_HPP
